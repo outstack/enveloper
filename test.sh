8de4ee42b866c75aca68479e47db21f546444484
Adam Quaile <adamquaile@gmail.com>
2017-06-03 13:23:22 +0100

    WIP. Travis builds

--- test.sh
+++ test.sh
@@ -4,5 +4,6 @@ set -e
 if [ ! -f phpunit.phar ]; then
     wget https://phar.phpunit.de/phpunit-5.7.phar -O ./phpunit.phar && chmod +x phpunit.phar
 fi
+./bin/console cache:clear --env=test && \
 ./phpunit.phar --filter=Unit && \
 ./phpunit.phar --filter=Functional
