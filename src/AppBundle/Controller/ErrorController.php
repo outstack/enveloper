b0f6f65cd5b7f002885d967095934498a7836dc8
Adam Quaile <adamquaile@gmail.com>
2017-06-11 19:04:09 +0100

    Removed unused parameter

--- src/AppBundle/Controller/ErrorController.php
+++ src/AppBundle/Controller/ErrorController.php
@@ -4,12 +4,10 @@ namespace AppBundle\Controller;
 
 use Outstack\Components\ApiProvider\ApiProblemDetails\ApiProblemFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
-use Zend\Diactoros\Response;
 
 class ErrorController extends Controller
 {
-    public function pageNotFoundAction(Request $request, ApiProblemFactory $problemFactory)
+    public function pageNotFoundAction(ApiProblemFactory $problemFactory)
     {
         return $problemFactory
             ->createProblem(404, 'Not Found')
