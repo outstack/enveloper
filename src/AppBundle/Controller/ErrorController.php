9c647d841025073be89a8da23037d1e387b0ae46
Adam Quaile <adamquaile@gmail.com>
2017-06-13 20:41:07 +0100

    Added handling of uncaught errors

--- src/AppBundle/Controller/ErrorController.php
+++ src/AppBundle/Controller/ErrorController.php
@@ -4,12 +4,41 @@ namespace AppBundle\Controller;
 
 use Outstack\Components\ApiProvider\ApiProblemDetails\ApiProblemFactory;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class ErrorController extends Controller
 {
+    /**
+     * @var ApiProblemFactory
+     */
+    private $problemFactory;
+
+    public function __construct(ApiProblemFactory $problemFactory)
+    {
+        $this->problemFactory = $problemFactory;
+    }
+
+    /**
+     * @Route("/errors/500")
+     */
+    public function serverErrorAction()
+    {
+        throw new \RuntimeException("Example exception");
+    }
+
+    public function showExceptionAction(FlattenException $exception)
+    {
+        return $this->problemFactory
+            ->createProblem(500, 'Server Error')
+            ->setDetail('An unexpected error occurred')
+            ->buildJsonResponse();
+
+    }
+
     public function pageNotFoundAction(ApiProblemFactory $problemFactory)
     {
-        return $problemFactory
+        return $this->problemFactory
             ->createProblem(404, 'Not Found')
             ->setDetail('No matching action was found to handle the request')
             ->buildJsonResponse();
