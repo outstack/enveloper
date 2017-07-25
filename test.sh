807ac9c648f0a4ff056281f3dde831d9b7df28db
Adam Quaile <adamquaile@gmail.com>
2017-07-25 22:13:23 +0100

    Debugging

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./composer.phar install --no-scripts --no-interaction --no-autoloader
+echo "" > var/logs/test.log
 ./bin/console cache:clear --env=test && \
 ./vendor/bin/simple-phpunit --filter=Unit && \
-./vendor/bin/simple-phpunit --filter=Functional
+./vendor/bin/simple-phpunit --filter=Functional || cat var/logs/test.log
