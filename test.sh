3cda98551546495a39c93fae953f4965cd825111
Adam Quaile <adamquaile@gmail.com>
2017-10-19 17:47:14 +0100

    Updated travis file

--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -e
+set -ev
 
 # Cleanup
 echo "" > var/logs/test.log
 
 COMPOSE="docker-compose -f ./docker-compose.yml -f ./docker-compose.test.yml"
 
+$COMPOSE run enveloper composer install --no-scripts --no-interaction --optimize-autoloader
 $COMPOSE run enveloper ./vendor/bin/simple-phpunit --filter=Unit
 $COMPOSE run enveloper \
     sh -c "./bin/console --env=test cache:warmup && ./vendor/bin/simple-phpunit --filter=Functional || cat var/logs/test.log"
