5c360b305d58bb145752863a968a0592a8c64698
Adam Quaile <aquaile@mashbo.com>
2018-05-23 10:27:12 +0100

    Updated tests to install composer, which is no longer in image

--- test.sh
+++ test.sh
@@ -7,6 +7,7 @@ echo "" > var/logs/test.log
 
 COMPOSE="docker-compose -f ./docker-compose.yml -f ./docker-compose.tests.yml"
 
+$COMPOSE run enveloper infrastructure/scripts/install-composer.sh
 $COMPOSE run enveloper ./composer.phar install --no-scripts --no-interaction --optimize-autoloader
 $COMPOSE run enveloper ./vendor/bin/simple-phpunit --filter=Unit
 $COMPOSE run enveloper \
--- test_travis.sh
+++ test_travis.sh
@@ -7,6 +7,7 @@ echo "" > var/logs/test.log
 
 COMPOSE="docker-compose -f ./docker-compose.yml -f ./docker-compose.travis.yml"
 
+$COMPOSE run enveloper infrastructure/scripts/install-composer.sh
 $COMPOSE run enveloper ./vendor/bin/simple-phpunit --filter=Unit
 $COMPOSE run enveloper \
     sh -c "./bin/console --env=test cache:warmup && ./vendor/bin/simple-phpunit --filter=Functional"
