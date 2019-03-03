c58bae281df805b999d68b42ff456195266ee007
Adam Quaile <adamquaile@gmail.com>
2019-03-03 13:16:44 +0000

    Allow taking hostname from proxies

--- web/app.php
+++ web/app.php
@@ -10,6 +10,13 @@ if (!$env) {
     $env = 'dev';
 }
 
+if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
+    Request::setTrustedProxies(explode(',', $trustedProxies), Request::HEADER_X_FORWARDED_ALL);
+}
+if ($trustedHosts = $_SERVER['TRUSTED_HOSTS'] ?? $_ENV['TRUSTED_HOSTS'] ?? false) {
+    Request::setTrustedHosts([$trustedHosts]);
+}
+
 $kernel = new AppKernel($env, in_array($env, ['dev', 'test']));
 
 $request = Request::createFromGlobals();
